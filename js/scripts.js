fa547c94246a18d1598561674cfa1ef78769c2ee
Alex Lucas <alexlucasxp@outlook.com>
2023-06-25 12:20:34 -0300

    bug correction 3

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ let altura = document.querySelector('#alturaField');
 const outDom = document.querySelector('#domOutput');
 
 // MÁSCARA DO INPUT: 'Altura'
-altura.addEventListener('keydown', () => {
+altura.addEventListener('keyup', () => {
     let alturaLength = altura.value.length;
     
     if (alturaLength === 1 ){
